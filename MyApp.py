7936a963ad2b844244c2286dad1035986b944f4f
Eli <eladnv@gmail.com>
2020-07-30 14:19:35 +0300

    Button 'open file' is working

--- MyApp.py
+++ MyApp.py
@@ -5,6 +5,12 @@ import os
 
 root = tk.Tk()
 
+
+def addApps():
+    fileName = filedialog.askopenfilename(
+        initialdir="/", title="Select file", filetypes=(("executables", "*.exe"), ("all files", "*.*")))
+
+
 canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
 canvas.pack()
 
@@ -12,7 +18,11 @@ frame = tk.Frame(root, bg="white")
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
 openFile = tk.Button(root, text="open file", padx=10,
-                     pady=5, fg="white", bg="#263D42")
+                     pady=5, fg="white", bg="#263D42", command=addApps)
 openFile.pack()
+
+runApps = tk.Button(root, text="Run Apps", padx=10,
+                    pady=5, fg="white", bg="#263D42")
+runApps.pack()
 
 root.mainloop()
